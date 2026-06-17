170464ce9d0d6089e509f18b410e28ad1ab9d133
smrutiranjankundu <smrutiranjankundu98@gmail.com>
2026-06-18 01:01:07 +0530

    newtc

--- tests/apiGet.spec.js
+++ tests/apiGet.spec.js
@@ -1,8 +1,10 @@
 import { expect, test } from "@playwright/test";
-import request from "node:test";
 
-test('Api get', async({request})=>{
-    const response= await request.get("https://parabank.parasoft.com/parabank/images/logo.gif");
-await expect(response.status()).toBe(200);
-await expect (response.ok()).toBeTruthy();
-})
+test("Api get", async ({ request }) => {
+  const response = await request.get(
+    "https://parabank.parasoft.com/parabank/images/logo.gif"
+  );
+
+  await expect(response.status()).toBe(200);
+  await expect(response.ok()).toBeTruthy();
+});
---
+++ tests/newtestcase.spec.js
@@ -0,0 +1,7 @@
+import {test, expect} from '@playwright/test';
+
+test('new test case', async ({page}) => {
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    await expect(page).toHaveTitle(/ParaBank/);
+    await page.waitForTimeout(3000);
+});
